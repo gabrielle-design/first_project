6f26ac9366e092517c5aed46bb5ff1b837c2b255
gabrielle-design <75559081+gabrielle-design@users.noreply.github.com>
2020-12-07 02:03:07 -0500

    Update first.py

--- first.py
+++ first.py
@@ -1,10 +1,14 @@
 # First project of the Joyita
 
-puwis_dogs = ['Joyita','Maya','Pelusa','Tofy','Popi']
+puwis_dogs = ['Joyita','Maya','Pelusa','Tofy','Popi', 'intruso']
 
 print ('Lindas Dogs are:')
 for x in puwis_dogs:
+    if x=="intruso":
+        print("You are not her dog")
+    Elseif:
     print(x)
+    
 
     
 
