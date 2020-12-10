7ade2372771bf286e0930d4fd38b81259a04dee4
Linda Gonzáles <lindagabriellegonzales@MacBook-Air-de-Linda.local>
2020-12-10 04:01:05 -0500

    yes, I'm commited

---
+++ stairs.py
@@ -0,0 +1,9 @@
+y={1,2,3,4,5,6}
+a= int(input("Write a number:"))
+b= int(input("Write another number:"))
+
+print("Your first number was: ",a)
+print("Your second number was: ",b)
+print("Sum:" ,a+b)
+print("Product: " ,a*b)
+print("Thank you for playing with me!!!!")
