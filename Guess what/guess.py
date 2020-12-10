6c8628889df707b5dc7d6ed85bef516b72fe5c76
Linda Gonzáles <lindagabriellegonzales@MacBook-Air-de-Linda.local>
2020-12-10 04:00:29 -0500

    another commit

---
+++ Guess what/guess.py
@@ -0,0 +1,35 @@
+print("let's play")
+print("Think a number")
+print("Don't tell me please")
+print("Let's call this number A")
+print("Now, add 5")
+print("multiply the result by 2")
+print("to what was left subtract 4")
+print("divide the result by 2")
+print("subtract the number you thought first")
+
+X=2
+A=(((((X+5)*2)-4))/2)-X
+
+print("is your answer ?",A)
+
+b=input("Do you wanna play again?")
+if b=="Yes":
+    print("Ok, let's go")
+else:
+    print("You are such a pussy")
+
+print("let's play")
+print("Think a number")
+print("Don't tell me please")
+print("Let's call this number B")
+print("Now, add 1")
+print("multiply the result by 2")
+print("add 7")
+print("divide the result by 2")
+print("subtract the number you thought first(B)")
+
+X=2
+B=(((((X+1)*2)+7))/2)-X
+
+print("is your answer ?",B)
